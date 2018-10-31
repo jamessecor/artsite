1d4cefc3ddd0057f6372f37308b577ef5bd98a19
James Secor <james.secor@gmail.com>
2018-10-30 21:07:26 -0400

    added current shows page

---
+++ shows.php
@@ -0,0 +1,81 @@
+<?php
+include "header.php";
+?>
+<div class="container">
+	<div id="success">
+		<div class="cv-text">
+
+<div class="row">
+	<div class="col-md-10 col-md-offset-1 center-it show-title">
+		Two New Shows by James Secor
+	</div>
+</div>
+	
+<!-- TEST -->
+<div class="row">
+	<div class="col-md-6 center-it ">
+		<img  class="current-show-img" src="http://jamessecor.com/img/Units%20AL-AP_2018.jpg"  alt="Could not retrieve image"/>
+		<p>
+			<strong><em><big><a href="./asnotseen" target="blank">As Not Seen</a></big></em></strong>
+			<br>November 20 - February 5 (2018-2019)
+			<br><a href="https://sterlingcollege.edu/academics/brown-library/" target="blank">Sterling College's Brown Library</a>
+			<br>N Craftsbury Rd, Craftsbury, VT			
+			<br><small>above: <em><strong>Units AL-AP</strong></em>, 2018. acrylic and collage on paper</small>	
+		</p>
+	</div>
+	<div class="col-md-6 center-it">
+		<img class="current-show-img" src="http://jamessecor.com/img/boston.jpg"  alt="Could not retrieve image"/>
+		<p>
+			<strong><em><big>Driving Up a Chimney</big></em></strong>
+			<br>Opens in December. <em>[dates TBD]</em>
+			<br><a href="http://cal-vt.org/index.html" target="blank"> The Center for Arts and Learning</a>
+			<br>46 Barre St, Montpelier, VT			
+			<br><small>above: <em><strong>At My Wit's End</strong></em>, 2018. acrylic on board</small>	
+		</p>
+	</div>
+</div>
+<!-- END TEST -->
+
+<!--
+<!-- AS NOT SEEN --
+<div class="row">
+	<div class="col-md-3 col-md-offset-3 center-it">
+		<img  src="../img/Units AV-AZ_2018.jpg"  alt="Could not retrieve image"/>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md center-it">
+		<p>
+			<strong><em><a href="./asnotseen" target="blank">As Not Seen</a></em></strong>
+			<br>at Sterling College's Brown Library
+			<br>N Craftsbury Rd, Craftsbury, VT
+			<br>Opens in November. <em>[dates TBD]</em>
+			<!--<br>November xx - January xx (2018-2019)--
+		</p>
+	</div>
+</div>
+
+<!-- Driving up a Chimney --
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+		<img class="current-show-img" src="../img/boston.jpg"  alt="Could not retrieve image"/>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md center-it">		
+		<p>
+			<strong><em>Driving Up a Chimney</em></strong>
+			<br>at the Center for Arts and Learning
+			<br>46 Barre St, Montpelier, VT
+			<br>Opens in December. <em>[dates TBD]</em>
+			<!--<br>November xx - January xx (2018-2019)--
+		</p>
+	</div>
+</div>
+-->
+		</div>
+	</div>
+</div>
+<?php
+include "footer.php";
+?>
