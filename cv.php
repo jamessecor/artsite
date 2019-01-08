746da3eea1d080fa825b77023d5547accc80d32f
James Secor <james.secor@gmail.com>
2019-01-07 21:28:34 -0500

    merged master

--- cv.php
+++ cv.php
@@ -6,22 +6,14 @@ include "header.php";
 		<div class="cv-text">
 			<p>
 				<strong>
-					Education
+					&#x25B6; Solo Exhibitions
 				</strong>
 			</p>
 
 			<p>
-				Hobart and William Smith Colleges, Geneva, NY - Honors project and Minor in Studio Art, 2010
-			</p>
-
-			<p>
-				<strong>
-					Solo Exhibitions
-				</strong>
-			</p>
-
-			<p>
-				<em>As Not Seen</em>, Art Hop (Site 43), Burlington, VT Sept 2018
+				<em>As Not Seen</em>, Center for Arts and Learning, Montpelier, VT Dec 2018-
+				<br><em>Driving Up a Chimney</em>, Brown Library, Craftsbury, VT, Nov 2018-
+				<br><em>As Not Seen</em>, Art Hop (Site 43), Burlington, VT Sept 2018
 				<br><em>Untitled</em>, Aldrich Public Library, Barre, VT, April 2018-
 				<br><em>#nomophobia</em>, Studio Place Arts, Barre, VT, Mar-Apr 2017
 				<br><em>In the Cracks</em>, The Blinking Light Gallery, Plainfield, VT, Jan-Feb 2017
@@ -33,7 +25,7 @@ include "header.php";
 			
 			<p>
 				<strong>
-					Selected Group Exhibitions
+					&#x25B6; Selected Group Exhibitions
 				</strong>
 			</p>
 
@@ -41,9 +33,9 @@ include "header.php";
 				<em>Summer Juried Show</em>, T.W. Wood Gallery, Montpelier, VT, 2017
 				<br><em>Encountering Yellow</em>, Studio Place Arts, Barre, VT, 2016
 				<br><em>Ongoing Member Shows</em>, The Front, Montpelier, VT, 2015-2017
-				<br><em>CELEBRATE!</em>, Studio Place Arts, Barre, VT, 2015 & 2016
+				<br><em>CELEBRATE!</em>, Studio Place Arts, Barre, VT, 2015 &amp; 2016
 				<br><em>The Montpelier Project</em>, City Center, Montpelier, VT, 2015
-				<br><em>HeArt & Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</em>, North End Studios, Burlington, VT, 2015
+				<br><em>HeArt &amp; Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</em>, North End Studios, Burlington, VT, 2015
 				<br><em>GallerySIX Group Show</em>, Montpelier, VT, 2015
 				<br><em>Art Resource Association Group Shows</em>, Montpelier, VT, 2012-2013
 				<br><em>20-30, 2D-3D</em>, Chandler Center for the Arts Gallery, Randolph, VT, 2013
@@ -52,12 +44,20 @@ include "header.php";
 			
 			<p>
 				<strong>
-					Residency Participation
+					&#x25B6; Residency Participation
+				</strong>
+			</p>
+			<p>
+				VT Artists Week, Vermont Studio Center, Johnson, VT, 2018 
+			</p>
+			<p>
+				<strong>
+					&#x25B6; Education
 				</strong>
 			</p>
 
 			<p>
-				VT Artists Week, Vermont Studio Center, Johnson, VT, 2018 
+				Hobart and William Smith Colleges, Geneva, NY - Honors project and Minor in Studio Art, 2010
 			</p>
 		</div>
 	</div>
