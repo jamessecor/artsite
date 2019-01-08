02c19698829a22222c8787a96df9b4899fb8f0e8
James Secor <james.secor@gmail.com>
2019-01-07 21:33:29 -0500

    fixed merge conflict

--- contact.php
+++ contact.php
@@ -113,10 +113,7 @@ if($validation=="Success") {
 			<td><textarea rows="4" class="full" name='memo' placeholder='Enter comments here.' ><?php echo isset($_POST['memo']) ? $_POST['memo']: '' ?></textarea></td>
 		</tr>
 		<tr><td><div class="g-recaptcha" data-sitekey="6LcZnoQUAAAAABN4yNqWK9PTKzdLHkvqMgquBnH1"></div></td></tr>
-<<<<<<< HEAD
-=======
 		<tr><td style="height:.25em;"></td></tr>
->>>>>>> captcha
 		<tr>
 			<td><small class="errorText"><?php echo array_key_exists('g-recaptcha-response', $errors) ? $errors['g-recaptcha-response'] : ""; ?></small></td>
 		</tr>
