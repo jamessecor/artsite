315124e1c263f4f9f9c3fe35a86ac8af69bde3fc
James Secor <james.secor@gmail.com>
2019-12-14 10:33:11 -0500

    bolded cv show titles

--- cv.php
+++ cv.php
@@ -11,16 +11,16 @@ include "header.php";
 			</p>
 
 			<p>
-				<em>As Not Seen</em>, Center for Arts and Learning, Montpelier, VT Dec 2018-Jan 2019
-				<br><em>Driving Up a Chimney</em>, Brown Library, Craftsbury, VT, Nov 2018-Jan 2019
-				<br><em>As Not Seen</em>, Art Hop (Site 43), Burlington, VT Sept 2018
-				<br><em>Untitled</em>, Aldrich Public Library, Barre, VT, April 2018-
-				<br><em>#nomophobia</em>, Studio Place Arts, Barre, VT, Mar-Apr 2017
-				<br><em>In the Cracks</em>, The Blinking Light Gallery, Plainfield, VT, Jan-Feb 2017
-				<br><em>Untitled</em>, The Daily Planet, Burlington, VT, Nov 2016-Jan 2017
-				<br><em>Local Scenery</em>, Greensboro Free Library, Greensboro, VT. March-May 2016
-				<br><em>Recent Work</em>, Skinny Pancake, Montpelier, VT, 2013
-				<br><em>Senegal-France-Syracuse</em>, Westcott Art Gallery, Syracuse, NY, 2011
+				<em><strong>As Not Seen</strong></em>, Center for Arts and Learning, Montpelier, VT Dec 2018-Jan 2019
+				<br><em><strong>Driving Up a Chimney</strong></em>, Brown Library, Craftsbury, VT, Nov 2018-Jan 2019
+				<br><em><strong>As Not Seen</strong></em>, Art Hop (Site 43), Burlington, VT Sept 2018
+				<br><em><strong>Untitled</strong></em>, Aldrich Public Library, Barre, VT, April 2018-
+				<br><em><strong>#nomophobia</strong></em>, Studio Place Arts, Barre, VT, Mar-Apr 2017
+				<br><em><strong>In the Cracks</strong></em>, The Blinking Light Gallery, Plainfield, VT, Jan-Feb 2017
+				<br><em><strong>Untitled</strong></em>, The Daily Planet, Burlington, VT, Nov 2016-Jan 2017
+				<br><em><strong>Local Scenery</strong></em>, Greensboro Free Library, Greensboro, VT. March-May 2016
+				<br><em><strong>Recent Work</strong></em>, Skinny Pancake, Montpelier, VT, 2013
+				<br><em><strong>Senegal-France-Syracuse</strong></em>, Westcott Art Gallery, Syracuse, NY, 2011
 			</p>
 			
 			<p>
@@ -30,17 +30,17 @@ include "header.php";
 			</p>
 
 			<p>
-				<em>Contemporary American Regionalism: Vermont Perspectives</em>, Southern Vermont Art Center, Manchester, VT, 2019
-				<br><em>Summer Juried Show</em>, T.W. Wood Gallery, Montpelier, VT, 2017
-				<br><em>Encountering Yellow</em>, Studio Place Arts, Barre, VT, 2016
-				<br><em>Ongoing Member Shows</em>, The Front, Montpelier, VT, 2015-2017
-				<br><em>CELEBRATE!</em>, Studio Place Arts, Barre, VT, 2015 &amp; 2016
-				<br><em>The Montpelier Project</em>, City Center, Montpelier, VT, 2015
-				<br><em>HeArt &amp; Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</em>, North End Studios, Burlington, VT, 2015
-				<br><em>GallerySIX Group Show</em>, Montpelier, VT, 2015
-				<br><em>Art Resource Association Group Shows</em>, Montpelier, VT, 2012-2013
-				<br><em>20-30, 2D-3D</em>, Chandler Center for the Arts Gallery, Randolph, VT, 2013
-				<br><em>Senior Show</em>, Houghton House, Geneva, NY, 2010
+				<em><strong>Contemporary American Regionalism: Vermont Perspectives</strong></em>, Southern Vermont Art Center, Manchester, VT, 2019
+				<br><em><strong>Summer Juried Show</strong></em>, T.W. Wood Gallery, Montpelier, VT, 2017
+				<br><em><strong>Encountering Yellow</strong></em>, Studio Place Arts, Barre, VT, 2016
+				<br><em><strong>Ongoing Member Shows</strong></em>, The Front, Montpelier, VT, 2015-2017
+				<br><em><strong>CELEBRATE!</strong></em>, Studio Place Arts, Barre, VT, 2015 &amp; 2016
+				<br><em><strong>The Montpelier Project</strong></em>, City Center, Montpelier, VT, 2015
+				<br><em><strong>HeArt &amp; Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</strong></em>, North End Studios, Burlington, VT, 2015
+				<br><em><strong>GallerySIX Group Show</strong></em>, Montpelier, VT, 2015
+				<br><em><strong>Art Resource Association Group Shows</strong></em>, Montpelier, VT, 2012-2013
+				<br><em><strong>20-30, 2D-3D</strong></em>, Chandler Center for the Arts Gallery, Randolph, VT, 2013
+				<br><em><strong>Senior Show</strong></em>, Houghton House, Geneva, NY, 2010
 			</p>
 			
 			<p>
