384f57102f6e84b128aee8c44eb70fefdcc401d1
James Secor <james.secor@gmail.com>
2020-03-11 22:02:05 -0400

    updated circles

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ include "./imagesTemplate.php";
 			</div>
 		<?php } ?>
 	</div>
+	<div id="help-me-thru">Touch/Click anywhere to proceed to paintings.</div>
 	<div id="images-main">
 		<?php
 		displayImages(" isHomePage = true ORDER BY arrangement");
@@ -30,11 +31,12 @@ var timesThruColors = 0;
 var idToPause = "colors-col-3-3";
 $(document).ready(function() {	
 	$("#images-main").css("display","none");	
+	$("#help-me-thru").css("display","none");
 	var rowHeight = $(".colors-col").css("width");
 	$(".navbar").css("margin","0");
 	$("#colors").children().css("height",rowHeight);
 	colors();
-	colorsInterval = setInterval(colors,3000);
+	colorsInterval = setInterval(colors,1500);
 });
 
 // Go to images
@@ -48,10 +50,17 @@ $(".colors-col").each(function() {
 	}
 });
 
+$("#help-me-thru").on("click",function() {
+	clearInterval(colorsInterval);
+	$("#colors").css("display","none");
+	$(this).css("display","none");
+	$("#images-main").css("display","");
+});
+
 // Pause Everything
 $("#" + idToPause).on("click",function() {
 	if(colorsInterval == null) {
-		colorsInterval = setInterval(colors,3000);
+		colorsInterval = setInterval(colors,1500);
 	} else {
 		clearInterval(colorsInterval);
 		colorsInterval = null;
@@ -59,22 +68,30 @@ $("#" + idToPause).on("click",function() {
 });
 
 function colors() {
+	var coords = [];
 	var transparency;
 	for(var i = 0; i < parseInt(<?php echo $rowCount; ?>); i++) {		
 		for(var j = 0; j < 12; j++) {			
-			transparency = Math.round(Math.random() * 2) ? Math.random() : "0";
-			var r = Math.round(Math.random() * 255);
-			var g = Math.round(Math.random() * 255);
-			var b = Math.round(Math.random() * 255);
-			var rgb = "rgba(" + r + "," + g + "," + b + "," + transparency + ")";
-			$("#colors-col-" + i + "-" + j).css("background-color",rgb).css("display","none");			
-			$("#colors-col-" + i + "-" + j).fadeIn(1500);			
-			$("#colors-col-" + i + "-" + j).fadeOut(1500);			
+			if(Math.random() * 10 < 2) {
+				coords.push(i + "-" + j);
+			} else {
+				$("#colors-col-" + i + "-" + j).css("display","").fadeOut(7000);			
+			}
 		}
 	}
+	coords.forEach(thisCircle => {
+		transparency = Math.round(Math.random() * 2) ? Math.random() : "0";
+		var r = Math.round(Math.random() * 255);
+		var g = Math.round(Math.random() * 255);
+		var b = Math.round(Math.random() * 255);
+		var rgb = "rgb(" + r + "," + g + "," + b + ")";
+		$("#colors-col-" + thisCircle).css("background-color",rgb);			
+		$("#colors-col-" + thisCircle).fadeIn(5000);
+	});
+	
 	timesThruColors++;
-	if(timesThruColors == 5) {
-		alert("Click anywhere to see paintings");
+	if(timesThruColors == 2) {
+		$("#help-me-thru").fadeIn(1000);
 	}
 }
 </script>
