56b9addd767029fe53ad5188e713ddf205ba76bd
James Secor <james.secor@gmail.com>
2020-03-15 13:58:03 -0400

    updated cv, better this time

--- cv.php
+++ cv.php
@@ -24,29 +24,18 @@ include "header.php";
 			</div>
 			<div id="solo">
 				<p>
-					<em><strong>Peanut Butter Garlic Toast</strong></em> - <a href="https://www.morseblockdeli.com/" class="press-links" target="blank">Morse Block Deli</a> - Barre, VT Dec 2019-
-					
-					<br><em><strong>animal mug, dish, and glass, etc</strong></em> - <a href="https://www.norwichlibrary.org/nplexhibits/" class="press-links" target="blank">Norwich Public Library</a> - Norwich, VT Dec 2019-
-					
-					<br><em><strong>As Not Seen</strong></em> - <a href="https://cal-vt.org/" class="press-links" target="blank">Center for Arts and Learning</a> - Montpelier, VT Dec 2018-Jan 2019
-					
-					<br><em><strong>Driving Up a Chimney</strong></em> - Brown Library - Craftsbury, VT, Nov 2018-Jan 2019
-					
-					<br><em><strong>As Not Seen</strong></em> - Art Hop (Site 43) - Burlington, VT Sept 2018
-					
-					<br><em><strong>Untitled</strong></em> - <a href="https://www.aldrichpubliclibrary.org/" class="press-links" target="blank">Aldrich Public Library</a> - Barre, VT, April 2018-
-					
-					<br><em><strong>#nomophobia</strong></em> - <a href="https://studioplacearts.com/" class="press-links" target="blank">Studio Place Arts</a> - Barre, VT, Mar-Apr 2017
-					
-					<br><em><strong>In the Cracks</strong></em> - The Blinking Light Gallery - Plainfield, VT, Jan-Feb 2017
-					
-					<br><em><strong>Untitled</strong></em> - <a href="http://dailyplanetvt.com/" class="press-links" target="blank">The Daily Planet</a> - Burlington, VT, Nov 2016-Jan 2017
-					
-					<br><em><strong>Local Scenery</strong></em> - <a href="https://www.greensborofreelibrary.org/" class="press-links" target="blank">Greensboro Free Library</a> - Greensboro, VT. March-May 2016
-					
-					<br><em><strong>Recent Work</strong></em> - Skinny Pancake - Montpelier, VT, 2013
-					
-					<br><em><strong>Senegal-France-Syracuse</strong></em> - Westcott Art Gallery - Syracuse, NY, 2011
+					<span class="show-titles">Peanut Butter Garlic Toast</span> <a href="https://www.morseblockdeli.com/" class="press-links" target="blank">Morse Block Deli</a> - Barre, VT Dec 2019-					
+					<br><span class="show-titles">animal mug, dish, and glass, etc</span> <a href="https://www.norwichlibrary.org/nplexhibits/" class="press-links" target="blank">Norwich Public Library</a> - Norwich, VT Dec 2019-
+					<br><span class="show-titles">As Not Seen</span> <a href="https://cal-vt.org/" class="press-links" target="blank">Center for Arts and Learning</a> - Montpelier, VT Dec 2018-Jan 2019
+					<br><span class="show-titles">Driving Up a Chimney</span> Brown Library - Craftsbury, VT, Nov 2018-Jan 2019
+					<br><span class="show-titles">As Not Seen</span> Art Hop (Site 43) - Burlington, VT Sept 2018
+					<br><span class="show-titles">Untitled</span> <a href="https://www.aldrichpubliclibrary.org/" class="press-links" target="blank">Aldrich Public Library</a> - Barre, VT, April 2018-
+					<br><span class="show-titles">#nomophobia</span> <a href="https://studioplacearts.com/" class="press-links" target="blank">Studio Place Arts</a> - Barre, VT, Mar-Apr 2017
+					<br><span class="show-titles">In the Cracks</span> The Blinking Light Gallery - Plainfield, VT, Jan-Feb 2017
+					<br><span class="show-titles">Untitled</span> <a href="http://dailyplanetvt.com/" class="press-links" target="blank">The Daily Planet</a> - Burlington, VT, Nov 2016-Jan 2017
+					<br><span class="show-titles">Local Scenery</span> <a href="https://www.greensborofreelibrary.org/" class="press-links" target="blank">Greensboro Free Library</a> - Greensboro, VT. March-May 2016
+					<br><span class="show-titles">Recent Work</span> Skinny Pancake - Montpelier, VT, 2013
+					<br><span class="show-titles">Senegal-France-Syracuse</span> Westcott Art Gallery - Syracuse, NY, 2011
 				</p>
 			</div>
 			<div id="group-header">	
@@ -58,17 +47,17 @@ include "header.php";
 			</div>
 			<div id="group">
 				<p>
-					<em><strong>Contemporary American Regionalism: Vermont Perspectives</strong></em> - Southern Vermont Art Center, Manchester, VT, 2019
-					<br><em><strong>Summer Juried Show</strong></em> - T.W. Wood Gallery, Montpelier, VT, 2017
-					<br><em><strong>Encountering Yellow</strong></em> - Studio Place Arts, Barre, VT, 2016
-					<br><em><strong>Ongoing Member Shows</strong></em> - The Front, Montpelier, VT, 2015-2017
-					<br><em><strong>CELEBRATE!</strong></em> - Studio Place Arts, Barre, VT, 2015 &amp; 2016
-					<br><em><strong>The Montpelier Project</strong></em> - City Center, Montpelier, VT, 2015
-					<br><em><strong>HeArt &amp; Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</strong></em> -  North End Studios, Burlington, VT, 2015
-					<br><em><strong>GallerySIX Group Show</strong></em> -  Montpelier, VT, 2015
-					<br><em><strong>Art Resource Association Group Shows</strong></em> -  Montpelier, VT, 2012-2013
-					<br><em><strong>20-30, 2D-3D</strong></em> -  Chandler Center for the Arts Gallery, Randolph, VT, 2013
-					<br><em><strong>Senior Show</strong></em> -  Houghton House, Geneva, NY, 2010
+					<span class="show-titles">Contemporary American Regionalism: Vermont Perspectives</span> Southern Vermont Art Center, Manchester, VT, 2019
+					<br><span class="show-titles">Summer Juried Show</span> T.W. Wood Gallery, Montpelier, VT, 2017
+					<br><span class="show-titles">Encountering Yellow</span> Studio Place Arts, Barre, VT, 2016
+					<br><span class="show-titles">Ongoing Member Shows</span> The Front, Montpelier, VT, 2015-2017
+					<br><span class="show-titles">CELEBRATE!</span> Studio Place Arts, Barre, VT, 2015 &amp; 2016
+					<br><span class="show-titles">The Montpelier Project</span> City Center, Montpelier, VT, 2015
+					<br><span class="show-titles">HeArt &amp; Home: Celebrating Inclusive Neighborhoods for Fair Housing Month</span>  North End Studios, Burlington, VT, 2015
+					<br><span class="show-titles">GallerySIX Group Show</span>  Montpelier, VT, 2015
+					<br><span class="show-titles">Art Resource Association Group Shows</span>  Montpelier, VT, 2012-2013
+					<br><span class="show-titles">20-30, 2D-3D</span>  Chandler Center for the Arts Gallery, Randolph, VT, 2013
+					<br><span class="show-titles">Senior Show</span>  Houghton House, Geneva, NY, 2010
 				</p>
 			</div>			
 			<p>
