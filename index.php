6974977d22902e040ec2a651062061406a4020ba
James Secor <james.secor@gmail.com>
2020-04-03 18:04:17 -0400

    finished store

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ include "./header.php";
 <br/>
 <div class="row store-row">
     <div class="col-md-4">
-        <img width="100%" src="http://jamessecor.com/img/AtMyWitsEnd2018.jpg"/>
+        <img width="100%" src="http://jamessecor.com/img/TreesontheNorthBranch2016.jpg"/>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
         <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="business" value="james.secor@gmail.com">
@@ -60,7 +60,43 @@ include "./header.php";
         <input type="hidden" name="option_amount2" value="20.00">
         <input type="hidden" name="option_index" value="0">
         </form>
-    </div>
+	</div>
+	<div class="col-md-4">
+        <img width="100%" src="http://jamessecor.com/img/teacup.jpg"/>
+        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+        <input type="hidden" name="cmd" value="_cart">
+        <input type="hidden" name="business" value="james.secor@gmail.com">
+        <input type="hidden" name="lc" value="US">
+        <input type="hidden" name="item_name" value="Postcards - Espresso at Art Hop">
+        <input type="hidden" name="item_number" value="5">
+        <input type="hidden" name="button_subtype" value="products">
+        <input type="hidden" name="no_note" value="0">
+        <input type="hidden" name="tax_rate" value="6.000">
+        <input type="hidden" name="shipping" value="0.00">
+        <input type="hidden" name="add" value="1">
+        <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest">
+        <table>        
+        <tr><td>Espresso at Art Hop</td></tr>
+        <tr><td><input type="hidden" name="on0" value="Postcards">4.5 x 5.5 inch postcards</td></tr><tr><td><select name="os0">
+            <option value="5 cards">5 cards $8.00 USD</option>
+            <option value="10 cards">10 cards $15.00 USD</option>
+            <option value="15 cards">15 cards $20.00 USD</option>
+		</select> </td></tr>
+		<tr><td>
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</td></tr>
+        </table>
+        <input type="hidden" name="currency_code" value="USD">
+        <input type="hidden" name="option_select0" value="5 cards">
+        <input type="hidden" name="option_amount0" value="8.00">
+        <input type="hidden" name="option_select1" value="10 cards">
+        <input type="hidden" name="option_amount1" value="15.00">
+        <input type="hidden" name="option_select2" value="15 cards">
+        <input type="hidden" name="option_amount2" value="20.00">
+        <input type="hidden" name="option_index" value="0">        
+        </form>
+	</div>	
     <div class="col-md-4">
         <img width="100%" src="http://jamessecor.com/img/IMG_20200308_112616.jpg"/>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
@@ -96,7 +132,9 @@ include "./header.php";
         <input type="hidden" name="option_amount2" value="20.00">
         <input type="hidden" name="option_index" value="0">
         </form>
-    </div>
+	</div>
+</div>
+<div class="row store-row">
     <div class="col-md-4">
         <img width="100%" src="http://jamessecor.com/img/animalMug_dish_andGlass_I.jpg"/>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
@@ -133,8 +171,6 @@ include "./header.php";
         <input type="hidden" name="option_index" value="0">
         </form>
 	</div>
-</div>
-<div class="row store-row">
     <div class="col-md-4">
         <img width="100%" src="http://jamessecor.com/img/barcodes2019.jpg"/>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
@@ -170,49 +206,15 @@ include "./header.php";
         <input type="hidden" name="option_amount2" value="20.00">
         <input type="hidden" name="option_index" value="0">
         </form>
-    </div>
-    <div class="col-md-4">
-        <img width="100%" src="http://jamessecor.com/img/teacup.jpg"/>
-        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
-        <input type="hidden" name="cmd" value="_cart">
-        <input type="hidden" name="business" value="james.secor@gmail.com">
-        <input type="hidden" name="lc" value="US">
-        <input type="hidden" name="item_name" value="Postcards - Espresso at Art Hop">
-        <input type="hidden" name="item_number" value="5">
-        <input type="hidden" name="button_subtype" value="products">
-        <input type="hidden" name="no_note" value="0">
-        <input type="hidden" name="tax_rate" value="6.000">
-        <input type="hidden" name="shipping" value="0.00">
-        <input type="hidden" name="add" value="1">
-        <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest">
-        <table>        
-        <tr><td>Espresso at Art Hop</td></tr>
-        <tr><td><input type="hidden" name="on0" value="Postcards">4.5 x 5.5 inch postcards</td></tr><tr><td><select name="os0">
-            <option value="5 cards">5 cards $8.00 USD</option>
-            <option value="10 cards">10 cards $15.00 USD</option>
-            <option value="15 cards">15 cards $20.00 USD</option>
-		</select> </td></tr>
-		<tr><td>
-			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-		</td></tr>
-        </table>
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="hidden" name="option_select0" value="5 cards">
-        <input type="hidden" name="option_amount0" value="8.00">
-        <input type="hidden" name="option_select1" value="10 cards">
-        <input type="hidden" name="option_amount1" value="15.00">
-        <input type="hidden" name="option_select2" value="15 cards">
-        <input type="hidden" name="option_amount2" value="20.00">
-        <input type="hidden" name="option_index" value="0">        
-        </form>
-	</div>
+    </div>    
 	<div class="col-md-4">
-		<img width="25%" src="http://jamessecor.com/img/AtMyWitsEnd2018.jpg"/>
-		<img width="25%" src="http://jamessecor.com/img/IMG_20200308_112616.jpg"/>
-        <img width="25%" src="http://jamessecor.com/img/animalMug_dish_andGlass_I.jpg"/>
-        <img width="25%" src="http://jamessecor.com/img/barcodes2019.jpg"/>
-        <img width="25%" src="http://jamessecor.com/img/teacup.jpg"/>
+		<span id="store-mix-pack-imgs">
+			<img width="20%" src="http://jamessecor.com/img/TreesontheNorthBranch2016.jpg"/>
+			<img width="20%" src="http://jamessecor.com/img/teacup.jpg"/>
+			<img width="20%" src="http://jamessecor.com/img/animalMug_dish_andGlass_I.jpg"/>
+			<img width="20%" src="http://jamessecor.com/img/IMG_20200308_112616.jpg"/>
+			<img width="20%" src="http://jamessecor.com/img/barcodes2019.jpg"/>			
+		</span>
         <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
         <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="business" value="james.secor@gmail.com">
